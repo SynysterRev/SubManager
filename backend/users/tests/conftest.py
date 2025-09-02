855cabada4f16cfb85112bf0e9b03b2e12fd5d7f
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-09-02 09:48:17 +0200

    create base project + custom user with tests

---
+++ backend/users/tests/conftest.py
@@ -0,0 +1,7 @@
+import pytest
+
+
+@pytest.fixture
+def user_model():
+    from django.contrib.auth import get_user_model
+    return get_user_model()
---
+++ backend/users/tests/test_models.py
@@ -0,0 +1,41 @@
+import pytest
+
+
+@pytest.mark.django_db
+def test_create_user(user_model):
+    user = user_model.objects.create_user(email="normal@user.com", password="foo")
+    assert user.email == "normal@user.com"
+    assert user.is_active
+    assert not user.is_staff
+    assert not user.is_superuser
+    try:
+        # username is None for the AbstractUser option
+        # username does not exist for the AbstractBaseUser option
+        assert user.username is None
+    except AttributeError:
+        pass
+    with pytest.raises(TypeError):
+        user_model.objects.create_user()
+    with pytest.raises(TypeError):
+        user_model.objects.create_user(email="")
+    with pytest.raises(ValueError):
+        user_model.objects.create_user(email="", password="foo")
+
+
+@pytest.mark.django_db
+def test_create_superuser(user_model):
+    admin_user = user_model.objects.create_superuser(email="super@user.com",
+                                                     password="foo")
+    assert admin_user.email == "super@user.com"
+    assert admin_user.is_active
+    assert admin_user.is_staff
+    assert admin_user.is_superuser
+    try:
+        # username is None for the AbstractUser option
+        # username does not exist for the AbstractBaseUser option
+        assert admin_user.username is None
+    except AttributeError:
+        pass
+    with pytest.raises(ValueError):
+        user_model.objects.create_superuser(
+            email="super@user.com", password="foo", is_superuser=False)
