e0e23f129b9c2eb18621ae7bc838426f80a6a51b
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-11-07 15:52:10 +0100

    Add missing migration

---
+++ backend/SubManager.Infrastructure/Migrations/20251107145149_CorrectCategoryName.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SubManager.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrectCategoryName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Name",
+                value: "News Media");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 10,
+                column: "Name",
+                value: "news_media");
+        }
+    }
+}
